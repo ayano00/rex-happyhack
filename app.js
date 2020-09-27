36b2745eec7ec5c1b11d5891866033e580fa256f
Ayano Oshima <oshimasayaka@MacBook-Air.local>
2020-09-28 04:12:43 +0900

    ver4

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-particleJS('particles-js', {
-    particles: {
-        color: '#fff',
-        shape: 'circle', // "circle", "edge" or "triangle"
-        opacity: 0.5,
-        size: 2,
-        size_random: true,
-        nb: 200,
-        line_linked: {
-            enable_auto: true,
-            distance: 250,
-            color: '#fff',
-            opacity: 0.5,
-            width: 1,
-            condensed_mode: {
-                enable: true,
-                rotateX: 600,
-                rotateY: 600
-            }
-        },
-        anim: {
-            enable: true,
-            speed: 2
-        }
-    },
-    interactivity: {
-        enable: false,
-        mouse: {
-            distance: 250
-        },
-        detect_on: 'canvas', // "canvas" or "window"
-        mode: 'grab'
-    },
-    /* Retina Display Support */
-    retina_detect: false
-});
+//particleJS('particles-js', {
+//    particles: {
+//        color: '#fff',
+//        shape: 'circle',
+//        opacity: 0.5,
+//        size: 2,
+//        size_random: true,
+//        nb: 200,
+//        line_linked: {
+//            enable_auto: true,
+//            distance: 250,
+//            color: '#fff',
+//            opacity: 0.5,
+//            width: 1,
+//            condensed_mode: {
+//                enable: true,
+//                rotateX: 600,
+//                rotateY: 600
+//            }
+//        },
+//        anim: {
+//            enable: true,
+//            speed: 2
+//        }
+//    },
+//    interactivity: {
+//        enable: false,
+//        mouse: {
+//            distance: 250
+//        },
+//        detect_on: 'canvas', 
+//        mode: 'grab'
+//    },
+//    retina_detect: false
+//});
